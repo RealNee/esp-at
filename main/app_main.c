d8825533a4dd60c7498788f4610c558b09a2d9ee
Nee <32289912+RealNee@users.noreply.github.com>
2023-04-22 17:17:31 +0800

    ..

--- main/app_main.c
+++ main/app_main.c
@@ -73,7 +73,7 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     at_nvs_flash_init_partition();
     printf("Hello World\n");
-    printf("This firmware is produced by Nee");
+    printf("This firmware is produced by Nee\n");
 
     at_netif_init();
 
